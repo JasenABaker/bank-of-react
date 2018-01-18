fe6435f67441d2c3a26715b93bcb0f2a3be6430d
JasenABaker <JasenABaker@gmail.com>
2018-01-18 18:26:17 -0500

    created debit list page

--- src/components/DebitList.js
+++ src/components/DebitList.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+import Debit from './Debit'
 
 
 
 class DebitList extends Component {
-
-
-
     render() {
+        const debitComponents = props.debits.map((debit, index)=>{
+            return <Debit
+                        description={debit.description}
+                        amount={debit.amount}
+                        date={debit.date}
+                        key={index}/>
+        })
         return (
             <div>
+                {debitComponents}
             </div>
         )
     }

612bfd3acf5a4491de6371eb743a8cf6aa57bbe0
JasenABaker <JasenABaker@gmail.com>
2018-01-18 19:13:37 -0500

    Added debitform page

---
+++ src/components/NewDebitForm.js
@@ -0,0 +1,62 @@
+import React, { Compponent, Component } from 'react'
+
+
+class NewDebitFrom extends Component {
+    state = {
+        newDebit:{
+            amount: 0.00,
+            description: '',
+            date: Date.now(),
+        }
+        
+    }
+
+    handleInputeChange =(event) =>{
+        const attribute = event.target.name
+        let value = event.target.value
+
+        if(attribute === 'amount') {
+            value = Number(value)
+        }
+    
+    const newDebit = {...this.state.newDebit}
+    newDebit[attribute] = value
+
+    this.setState({newDebit})
+    }
+
+    resetForm = () => {
+        const newDebit = {...this.defaultState.newDebit}
+        this.setState({newDebit})
+    }
+
+    addNewDebit = (event) => {
+        event.preventDefault()
+        this.props.addNewDebit(this.state.newDebit)
+        this.resetForm()
+    }
+    render(){
+        return(
+            <div>
+                <form onSubmit={this.addNewDebit}>
+                <div>
+                    <span>Amount (USD) </span>
+                    <input
+                        type="number"
+                        name="amount"
+                        placeholder="Amount"
+                        value={this.state.newDebit.amount}
+                        onChange={this.handleInputeChange}/>
+                    </div>
+                    <div>
+                        <input
+                            type="submit"
+                            value="Add New Debit"/>
+                    </div>
+
+            </form>
+            </div>
+        )
+    }
+
+}
